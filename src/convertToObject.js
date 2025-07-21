3fd11aa4b89d9b164661fded063dc9a725cbd804
marcoduartestorm <marcosbottocomp@gmail.com>
2025-07-21 15:26:34 -0300

    Solução

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let vetSplited = sourceString.split(';')
+  const obj = {};
+
+  for (const c of vetSplited) {
+    let c2;
+
+    if (c !==  undefined) {
+      c2 = c.trim();
+    }
+
+    if (c2 !== '') {
+      let registerSplited = c.split(':');
+      const key = registerSplited[0].trim();
+      const value = registerSplited[1].trim();
+      Object.assign(obj, {[key]:value});
+    }
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;
